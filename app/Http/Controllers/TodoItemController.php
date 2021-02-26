de046a7e6c2b9a3bb5f83fade6808450c8488a32
saulamsal <saulamsal@gmail.com>
2021-02-25 19:17:24 -0600

    todoList+ todoitem Model+Controller+rsc created

---
+++ app/Http/Controllers/TodoItemController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TodoItem;
+use Illuminate\Http\Request;
+
+class TodoItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TodoItem  $todoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TodoItem $todoItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TodoItem  $todoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TodoItem $todoItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TodoItem  $todoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TodoItem $todoItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TodoItem  $todoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TodoItem $todoItem)
+    {
+        //
+    }
+}
