92b5ca45c80f4f79da361784fc594b378b91b00e
saulamsal <saulamsal@gmail.com>
2021-02-25 19:34:06 -0600

    add simple header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,11 @@
 @extends('app')
 @section('content')
-Test
+
+
+<div class="w-100  d-flex justify-content-center align-items-center flex-column">
+    <h1 class="w-100 mb-4 text-center">Guideline Center toDo</h1>
+</div>
+
+
+
 @endsection
